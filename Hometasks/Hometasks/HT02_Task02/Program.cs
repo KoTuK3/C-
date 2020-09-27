ab11592edefac920120e73d0aaf244de30a7c4d7
Anton Menchinskii <menchynskyi_ak19@nuwm.edu.ua>
2020-09-27 13:05:49 +0300

    Added HT02

---
+++ Hometasks/Hometasks/HT02_Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT02_Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
