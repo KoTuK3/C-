e643b6182ba23f010ba0c4e7936e9a90ae30861b
Anton Menchinskii <anton.menchinskii@gmail.com>
2020-11-04 21:07:07 +0200

    Added Lesson08

---
+++ WinFormsLessons/Lessons/L08_Task01/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace L08_Task01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // DrawPanel
+            // 
+            this.DrawPanel.Location = new System.Drawing.Point(12, 12);
+            this.DrawPanel.Name = "DrawPanel";
+            this.DrawPanel.Size = new System.Drawing.Size(776, 563);
+            this.DrawPanel.TabIndex = 0;
+            this.DrawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawPanel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.DrawPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel DrawPanel;
+    }
+}
+
---
+++ WinFormsLessons/Lessons/L08_Task01/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L08_Task01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            //Sky
+            Rectangle skyRect = new Rectangle(0, 0, DrawPanel.Width, DrawPanel.Height / 5 * 3);
+
+            e.Graphics.FillRectangle(Brushes.SkyBlue, skyRect);
+
+            //Ground
+            Rectangle groundRect = new Rectangle(0, DrawPanel.Height / 5 * 3, DrawPanel.Width, DrawPanel.Height / 5 * 3 + 1);
+
+            e.Graphics.FillRectangle(Brushes.DarkGreen, groundRect);
+
+            // Wall
+            var xCenter = DrawPanel.Width / 2;
+            var yCenter = DrawPanel.Height / 2;
+            var wallSize = 300;
+            var wallRect = new Rectangle(xCenter - wallSize / 2, yCenter - wallSize / 2, wallSize, wallSize);
+
+            e.Graphics.FillRectangle(Brushes.Brown, wallRect);
+
+            // Roof
+            int roofHeight = wallSize / 3;
+            int roofWidth = wallSize + 40;
+
+            Point[] points = {
+                new Point(xCenter + roofWidth / 2 + 20, yCenter - wallSize / 2),
+                new Point(xCenter, yCenter - wallSize / 2  - roofHeight),
+                new Point(xCenter - roofWidth / 2 - 20, yCenter - wallSize / 2)
+            };
+
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddCurve(points, 0);
+
+            e.Graphics.FillPath(Brushes.DarkRed, path);
+
+            // Door
+            int doorWidth = wallSize / 4;
+            int doorHeight = wallSize / 2;
+
+            Rectangle doorRect = new Rectangle(xCenter - (wallSize - doorWidth) / 2, yCenter + wallSize / 2 - doorHeight, doorWidth, doorHeight);
+
+            e.Graphics.FillRectangle(Brushes.Gray, doorRect);
+
+            //Window            
+            Image img = Image.FromFile(@"D:\Step\CSharp\WinFormsLessons\Lessons\L08_Task01\Images\WindowImage.png");
+            Rectangle windorRect = new Rectangle(xCenter, yCenter - wallSize / 4, wallSize / 2 - 20, wallSize / 4);
+
+            e.Graphics.FillRectangle(Brushes.SkyBlue, windorRect);
+            e.Graphics.DrawImage(img, windorRect);
+        }
+    }
+}
