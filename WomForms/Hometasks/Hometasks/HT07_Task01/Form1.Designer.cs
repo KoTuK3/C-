75223f244444b106851332cadbdf5c3234a2f6a0
Anton Menchinskii <anton.menchinskii@gmail.com>
2020-11-12 20:20:41 +0200

    Added HT07 WinForms

---
+++ WomForms/Hometasks/Hometasks/HT07_Task01/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace HT07_Task01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPanel = new System.Windows.Forms.Panel();
+            this.CircleBtn = new System.Windows.Forms.Button();
+            this.SquareBtn = new System.Windows.Forms.Button();
+            this.TriangleBtn = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.FillBtn = new System.Windows.Forms.Button();
+            this.BorderBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DrawPanel
+            // 
+            this.DrawPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DrawPanel.Location = new System.Drawing.Point(13, 66);
+            this.DrawPanel.Name = "DrawPanel";
+            this.DrawPanel.Size = new System.Drawing.Size(775, 372);
+            this.DrawPanel.TabIndex = 0;
+            this.DrawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawPanel_Paint);
+            this.DrawPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DrawPanel_MouseClick);
+            // 
+            // CircleBtn
+            // 
+            this.CircleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CircleBtn.Location = new System.Drawing.Point(13, 12);
+            this.CircleBtn.Name = "CircleBtn";
+            this.CircleBtn.Size = new System.Drawing.Size(75, 48);
+            this.CircleBtn.TabIndex = 1;
+            this.CircleBtn.Text = "Circle";
+            this.CircleBtn.UseVisualStyleBackColor = true;
+            this.CircleBtn.Click += new System.EventHandler(this.CircleBtn_Click);
+            // 
+            // SquareBtn
+            // 
+            this.SquareBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SquareBtn.Location = new System.Drawing.Point(106, 12);
+            this.SquareBtn.Name = "SquareBtn";
+            this.SquareBtn.Size = new System.Drawing.Size(75, 48);
+            this.SquareBtn.TabIndex = 2;
+            this.SquareBtn.Text = "Square";
+            this.SquareBtn.UseVisualStyleBackColor = true;
+            this.SquareBtn.Click += new System.EventHandler(this.SquareBtn_Click);
+            // 
+            // TriangleBtn
+            // 
+            this.TriangleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TriangleBtn.Location = new System.Drawing.Point(197, 12);
+            this.TriangleBtn.Name = "TriangleBtn";
+            this.TriangleBtn.Size = new System.Drawing.Size(84, 48);
+            this.TriangleBtn.TabIndex = 3;
+            this.TriangleBtn.Text = "Triangle";
+            this.TriangleBtn.UseVisualStyleBackColor = true;
+            this.TriangleBtn.Click += new System.EventHandler(this.TriangleBtn_Click);
+            // 
+            // FillBtn
+            // 
+            this.FillBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FillBtn.Location = new System.Drawing.Point(551, 12);
+            this.FillBtn.Name = "FillBtn";
+            this.FillBtn.Size = new System.Drawing.Size(75, 48);
+            this.FillBtn.TabIndex = 4;
+            this.FillBtn.Text = "Fill color";
+            this.FillBtn.UseVisualStyleBackColor = true;
+            this.FillBtn.Click += new System.EventHandler(this.FillBtn_Click);
+            // 
+            // BorderBtn
+            // 
+            this.BorderBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BorderBtn.Location = new System.Drawing.Point(632, 12);
+            this.BorderBtn.Name = "BorderBtn";
+            this.BorderBtn.Size = new System.Drawing.Size(75, 48);
+            this.BorderBtn.TabIndex = 5;
+            this.BorderBtn.Text = "Border color";
+            this.BorderBtn.UseVisualStyleBackColor = true;
+            this.BorderBtn.Click += new System.EventHandler(this.BorderBtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClearBtn.Location = new System.Drawing.Point(713, 12);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 48);
+            this.ClearBtn.TabIndex = 6;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.BorderBtn);
+            this.Controls.Add(this.FillBtn);
+            this.Controls.Add(this.TriangleBtn);
+            this.Controls.Add(this.SquareBtn);
+            this.Controls.Add(this.CircleBtn);
+            this.Controls.Add(this.DrawPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel DrawPanel;
+        private System.Windows.Forms.Button CircleBtn;
+        private System.Windows.Forms.Button SquareBtn;
+        private System.Windows.Forms.Button TriangleBtn;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button FillBtn;
+        private System.Windows.Forms.Button BorderBtn;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
+
---
+++ WomForms/Hometasks/Hometasks/HT07_Task01/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HT07_Task01
+{
+    
+    
+    public partial class Form1 : Form
+    {
+        enum Figure
+        {
+            Circle,
+            Square,
+            Triangle
+        }
+        struct Coord
+        {
+            public int x;
+            public int y;
+        }
+
+        private Color _borderColor = Color.Black;
+        private Color _fillColor = Color.Black;
+        private int _size = 50;
+
+        List<FigureClass> _circleCoords = new List<FigureClass>();
+        List<FigureClass> _squaresCoords = new List<FigureClass>();
+        List<FigureClass> _triangesCoords = new List<FigureClass>();
+
+        Figure _figure;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CircleBtn_Click(object sender, EventArgs e)
+        {
+            _figure = Figure.Circle;
+        }
+
+        private void SquareBtn_Click(object sender, EventArgs e)
+        {
+            _figure = Figure.Square;
+        }
+
+        private void TriangleBtn_Click(object sender, EventArgs e)
+        {
+            _figure = Figure.Triangle;
+        }
+
+        private void DrawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+            foreach (var item in _circleCoords)
+            {
+                int size = item.size;
+                SolidBrush fillBrush = new SolidBrush(item.fillColor);
+                var borderPen = new Pen(item.borderColor);
+
+                e.Graphics.DrawArc(borderPen, item.x - size / 2, item.y - size / 2, item.size, item.size, 0, 360);
+                e.Graphics.FillEllipse(fillBrush, item.x - size / 2, item.y - size / 2, item.size, item.size);
+            }
+
+            foreach (var item in _squaresCoords)
+            {
+                int size = item.size;
+                SolidBrush fillBrush = new SolidBrush(item.fillColor);
+                var borderPen = new Pen(item.borderColor);
+
+                e.Graphics.DrawRectangle(borderPen, item.x - size / 2, item.y - size / 2, size, size);
+                e.Graphics.FillRectangle(fillBrush, item.x - size / 2, item.y - size / 2, size, size);
+            }
+        }
+
+        private void DrawPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            var figure = new FigureClass 
+            { 
+                x = e.X,
+                y = e.Y,
+                borderColor = _borderColor,
+                fillColor = _fillColor,
+                size = _size 
+            };
+
+            switch (_figure)
+            {
+                case Figure.Circle:
+                    _circleCoords.Add(figure);
+                    break;
+                case Figure.Square:
+                    _squaresCoords.Add(figure);
+                    break;
+                case Figure.Triangle:
+                    _triangesCoords.Add(figure);
+                    break;
+                default:
+                    break;
+            }
+
+            DrawPanel.Update();
+            DrawPanel.Refresh();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            _circleCoords.Clear();
+            _squaresCoords.Clear();
+            _triangesCoords.Clear();
+
+            DrawPanel.Update();
+            DrawPanel.Refresh();
+        }
+
+        private void BorderBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                _borderColor = cd.Color;
+            }
+        }
+
+        private void FillBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                _fillColor = cd.Color;
+            }
+        }
+    }
+    class FigureClass
+    {
+        public int size;
+        public int x;
+        public int y;
+        public Color borderColor;
+        public Color fillColor;
+    }
+}
